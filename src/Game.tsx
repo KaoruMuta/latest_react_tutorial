212334510a61d97e880a851a272d1f87e1a644cf
muta <muttaroni8827@gmail.com>
2021-08-17 20:47:18 +0900

    feat: finish Adding time travel

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,12 +1,72 @@
 import React, { FC } from 'react';
 import './Game.css';
 import { Board } from './components/Board';
+import { useState } from 'react';
+import { calculateWinner } from './helpers/CalculateWinnerHelper';
+
+type History = {
+  squares: (string | null)[];
+};
 
 const Game: FC = () => {
+  const [history, setHistory] = useState<History[]>([
+    {
+      squares: Array(9).fill(null),
+    },
+  ]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
+
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+  const moves = history.map((step, move) => {
+    const desc = move ? 'Go to move #' + move : 'Go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  const status = winner
+    ? 'Winner:' + winner
+    : 'Next player:' + (xIsNext ? 'X' : 'O');
+
+  const handleClick = (i: number) => {
+    const newHistory = history.slice(0, stepNumber + 1);
+    const current = newHistory[newHistory.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? 'X' : 'O';
+    setHistory(
+      newHistory.concat([
+        {
+          squares: squares,
+        },
+      ])
+    );
+    setStepNumber(newHistory.length);
+    setXIsNext(!xIsNext);
+  };
+
+  const jumpTo = (step: number) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board
+          squares={current.squares}
+          onClick={(i: number) => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
